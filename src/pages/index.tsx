6fd4aabe945a807f026d25b59cd1fdaab2cb2613
joshijax <joshijax08@gmail.com>
2025-09-04 22:25:09 +0100

    commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,7 +31,7 @@ const NewPage: React.FC = () => {
   return (
     <>
       <Head>
-        <title>BOUND - The Professional DeFi Yield Platform</title>
+        <title>BOUND - Decentralized Stablecoin Protocol</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
